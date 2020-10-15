a2328dc5d84e8951823e1b309fb99ebd02db0ba4
AA <aa@bigdumb.gq>
2020-10-14 22:40:42 -0400

    stuf

---
+++ index.js
@@ -0,0 +1,37 @@
+const { Plugin } = require('powercord/entities');
+const { getModule } = require('powercord/webpack');
+
+
+module.exports = class StereoCord extends Plugin {
+  startPlugin () {
+    this.VoiceConnection = getModule([ 'getVoiceEngine' ], false).getVoiceEngine().VoiceConnection;
+    class Stereo extends this.VoiceConnection {
+      constructor (a, b, c, d, e) {
+        super(a, b, c, d, e);
+        this.origin = super.setTransportOptions;
+      }
+
+      setTransportOptions (obj) {
+        if (obj.audioEncoder) {
+          obj.audioEncoder.params = { stereo: '1' };
+          obj.audioEncoder.channels = 2;
+        }
+        if (obj.fec) {
+          obj.fec = false;
+        }
+        if (obj.encodingVoiceBitRate < 448) {
+          obj.encodingVoiceBitRate = 448;
+        }
+
+
+        this.origin(obj);
+        window.sound = this;
+      }
+    }
+    getModule([ 'getVoiceEngine' ], false).getVoiceEngine().VoiceConnection = Stereo;
+  }
+
+  pluginWillUnload () {
+    getModule([ 'getVoiceEngine' ], false).getVoiceEngine().VoiceConnection = this.VoiceConnection;
+  }
+};
---
+++ readme.md
@@ -0,0 +1,1 @@
+stereocord but for powercord
